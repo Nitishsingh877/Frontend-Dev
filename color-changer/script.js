4c53886137608381045cf3500273a9cdcb548ac0
Nitishsingh877 <112621110+Nitishsingh877@users.noreply.github.com>
2023-04-03 14:14:17 +0530

    Add files via upload
    color-changer DOM

---
+++ color-changer/script.js
@@ -0,0 +1,16 @@
+const btn = document.getElementById("button");
+const randomColor = () => {
+    let val = "1234567890ABCDEF";
+    let cons = "#";
+    for(let i=0; i<6; i++){
+        cons = cons + val[Math.floor(Math.random() *16)];
+    }
+    return cons;
+};
+
+console.log(randomColor());
+
+function changerandomcolor() {
+    document.body.style.backgroundColor = randomColor()
+}
+btn.addEventListener("click", changerandomcolor);
