c1b19d1d82a5881f8f1ab74289ed87e06f9c1a8f
Nitish Kumar Singh <nitishkumarsingh877@gmail.com>
2023-04-04 16:10:50 +0530

    Add files via upload

---
+++ addeventlistner/onclick/onclick.js
@@ -0,0 +1,11 @@
+const btn = document.getElementById("button");
+
+btn.addEventListener("click", function () {
+          btn.innerText = 'clicked';
+          btn.style.backgroundColor = "blue";
+
+
+
+});
+
+
